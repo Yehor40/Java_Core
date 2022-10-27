1592dc4d23824e14a548c2fcb5a156225a1ea2f1
Yehor40 <fraugrey3@gmail.com>
2022-10-27 13:51:51 +0200

    Initial commit

---
+++ src/main/java/org/collections/Main.java
@@ -0,0 +1,48 @@
+package org.collections;
+import java.util.*;
+//Map stores key->value
+public class Main {
+
+    public static void main(String[] args) {
+        //hashmap implementation
+       Map<Integer,String>map = new HashMap<>();
+
+       map.put(1,"Hello");
+       map.put(2,"My");
+       map.put(3,"World");
+      /*
+       System.out.println(map);
+       map.put(1,"World");
+       System.out.println(map);
+
+       System.out.println(map.get(2));
+        System.out.println(map.get(4));
+       */
+       /*
+        for (Map.Entry<Integer,String> entry:map.entrySet()) {
+        System.out.println(entry.getKey()+"-"+entry.getValue());}
+        */
+
+
+        //natural ordering
+        Map <Integer,String> hashMap = new HashMap<>();//pair order is not defined
+        Map <Integer,String> linkedhashMap = new LinkedHashMap<>();//pair order is defined(the order they were added in is the order they will return)
+        Map <Integer,String> treeMap = new TreeMap<>();//pair is sorted by key
+        System.out.println("HashMap order:");
+        testMap(hashMap);
+        System.out.println("LinkedHashMap order:");
+        testMap(linkedhashMap);
+        System.out.println("TreeMap order order:");
+        testMap(treeMap);
+    }
+
+    public static void testMap(Map<Integer,String>map){
+        map.put(3,"dick");
+        map.put(8,"dic");
+        map.put(6,"di");
+        map.put(1,"d");
+        for (Map.Entry<Integer,String>entry: map.entrySet()) {
+            System.out.println(entry.getKey()+" "+entry.getValue());
+        }
+    }
+}
---
+++ src/main/java/org/collections/set.java
@@ -0,0 +1,47 @@
+package org.collections;
+
+import java.util.*;
+
+//set stores unique objects
+public class set {
+
+    public static void main(String[] args) {
+        Set<String> hashSet = new HashSet<>();//no order
+        Set<String> LinkedhashSet = new LinkedHashSet<>();//same order when it was put in
+        Set<String> treeSet = new TreeSet<>();//sorted order
+        hashSet.add("Dmytro");
+        hashSet.add("Ivan");
+        hashSet.add("Iskander");
+        hashSet.add("Iosif");
+        hashSet.add("Iosif");
+
+        Set<Integer> Set1 = new HashSet<>();
+
+        Set1.add(1);
+        Set1.add(2);
+        Set1.add(3);
+        Set1.add(4);
+        Set1.add(5);
+
+        Set<Integer> Set2 = new HashSet<>();
+        Set2.add(3);
+        Set2.add(4);
+        Set2.add(5);
+        Set2.add(6);
+        Set2.add(7);
+        Set<Integer> union = new HashSet<>(Set1);//no order
+        //union - union of set
+        union.addAll(Set2);
+        System.out.println(union);
+
+        //intersection - intersection of sets
+        Set<Integer> intersection = new HashSet<>(Set1);//
+        intersection.retainAll(Set2);
+        System.out.println(intersection);
+
+        //difference - difference of sets
+        Set<Integer> difference = new HashSet<>(Set1);//
+        difference.removeAll(Set2);
+        System.out.println(difference);
+    }
+}
